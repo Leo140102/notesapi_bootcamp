3a5da2254e9330f2fd3d80725055b7844c7a0f09
Leo140102 <leonardoclementedesouza@hotmail.com>
2021-11-17 22:36:45 -0200

    teste

---
+++ rotas/Notas.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", function(req, res){
+    res.send([]);
+});
+
+router.post("/", function(req, res){
+    res.send({});
+});
+router.put("/", function(req, res){
+    res.send({});
+});
+router.delete("/", function(req, res){
+    res.send({});
+});
+
+module.exports = router;
+
+
